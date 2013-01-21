efc7ee4920cafd71da119221a95ef99a87a24068
Riccardo Di Sipio <disipio@cern.ch>
2013-01-21 13:28:48 +0000

    directory tree. does not compile yet

---
+++ src/INtupleWrapper.cxx
@@ -0,0 +1,91 @@
+#include "INtupleWrapper.h"
+
+INtupleWrapper::INtupleWrapper( const char * fileListName, const char * branchListName, const char * treeName ) : 
+  m_hm(NULL),
+  m_treeName( treeName ), m_thisEvent(NULL), m_thisEventNumber(-1), m_maxEvents(-1)
+{
+  if( !SetupTools() ) throw runtime_error( "Cannot setup wrapper tools\n" );
+}
+ 
+
+INtupleWrapper::~INtupleWrapper()
+{
+}
+
+
+//////////////////////////////////////////
+
+
+bool INtupleWrapper::SetupTools()
+{
+  bool success = true;
+
+  m_hm         = HistogramManager::GetHandle();
+
+  return success;
+}
+
+
+//////////////////////////////////////////
+
+
+CutFlow * INtupleWrapper::AddCutFlow( const string& name )
+{
+  CutFlowFactory * factory = CutFlowFactory::GetHandle();
+
+  if( !factory ) throw runtime_error( "Cannot get handle to cutflow factory\n" );
+
+  CutFlow * p_cf = factory->Create( name );
+
+  if( !p_cf ) {
+    char err[128];
+    sprintf( err, "Cannot cutflow %s not registered\n", name.c_str() );
+  }
+
+  m_cutFlows[name] = p_cf;
+
+  return m_cutFlows[name];
+}
+
+
+//////////////////////////////////////////
+
+void INtupleWrapper::Loop( Long64_t nEventsMax )
+{
+  m_maxEvents = nEventsMax - 1;
+
+  m_thisEventNumber = 0; // default = uninitialized = -1
+
+  for( Long64_t i = 0 ; i < m_maxEvents ; ++i ) {
+    m_thisEvent = NextEvent();
+
+    if( !m_thisEvent ) throw runtime_error( "Cannot create event\n" );
+
+    for( CutFlowCollection_t::const_iterator itr = m_cutFlows.begin() ; itr != m_cutFlows.end() ; ++itr ) {
+      itr->second->Apply( m_thisEvent );
+    }
+  }
+
+}
+
+//////////////////////////////////////////
+
+
+EventData * INtupleWrapper::NextEvent()
+{
+   if( m_thisEvent ) delete m_thisEvent;
+
+   m_thisEvent = MakeEvent( ++m_thisEventNumber ); // must be implemented at some point downstream
+
+   if( ( m_thisEventNumber % int(m_maxEvents/10) ) == 0 ) {
+     double perc = 100. * m_thisEventNumber / m_maxEvents;
+     printf( "INFO: Event %i (n = %i, r = %i ) (%3.0f %%)\n", m_thisEventNumber, m_thisEvent->info.eventNumber, m_thisEvent->info.runNumber, perc );
+   }
+
+   return m_thisEvent;
+}
+
+
+//////////////////////////////////////////
+
+
---
+++ src/INtupleWrapper.h
@@ -0,0 +1,54 @@
+#ifndef __INTUPLE_WRAPPER_H__
+#define __INTUPLE_WRAPPER_H__
+
+#include "Commons.h"
+#include "EventData.h"
+#include "CutFlowFactory.hpp"
+#include "HistogramManager.h"
+
+//#include "NtupleWrapperFactory.hpp"
+
+class INtupleWrapper 
+{
+ public:
+   INtupleWrapper( const char * fileListName, const char * branchListName = "branch_list.txt", const char * treeName = "physics" );
+   virtual ~INtupleWrapper();
+
+ public:
+
+   virtual void        Loop( Long64_t nEventsMax );
+
+   virtual CutFlow   * AddCutFlow( const string& name );
+
+   virtual EventData * MakeEvent( Long64_t i ) = 0;
+
+   virtual Long64_t GetEntries() = 0;
+
+ protected:
+   bool                SetupTools();
+   virtual bool        LoadChain( const char * fileListName, const char * treeName = "physics" ) = 0;
+   virtual UInt_t      SetActiveBranches( const char * listFileName = "branch_list.txt" )        = 0;
+
+   virtual EventData  * NextEvent();
+
+   virtual bool         MakeEventInfo( EventData * ed )      = 0;
+   virtual bool         MakeEventMET( EventData * ed )       = 0;
+   virtual bool         MakeEventElectrons( EventData * ed ) = 0;
+   virtual bool         MakeEventMuons( EventData * ed )     = 0;
+   virtual bool         MakeEventJets( EventData * ed )      = 0;
+
+ protected:
+   HistogramManager * m_hm;
+
+   const char       * m_treeName;
+   set<string>        m_activeBranches;
+
+   Long64_t           m_thisEventNumber;
+   Long64_t           m_maxEvents;
+   EventData        * m_thisEvent;
+
+   CutFlowCollection_t m_cutFlows;
+};
+
+
+#endif /** __INTUPLE_WRAPPER_H__ */
