06c61d6f2dfee6451d561922fb0c9b8ec7cd20fb
Jiri Kvita <jiri.kvita@cern.ch>
2014-10-09 09:44:50 +0000

    removed MAMbo-setenv.sh from version control

--- bin/MAMbo-setenv.sh
+++
@@ -1,6 +0,0 @@
-export MAMBODIR=/scratch0/giannell/MAMbo/trunk
-source /scratch0/giannell/MAMbo/trunk/bin/MAMbo-autocompletion.sh
-
-export PATH=${PATH}:${MAMBODIR}/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MAMBODIR}/lib
-
