0351400d35935ecad2d5670fe3ca39ac4b7ec5fd
Riccardo Di Sipio <disipio@cern.ch>
2015-02-19 19:47:32 +0000

    support for alternate signal samples

--- bin/MAMbo-setenv.sh
+++
@@ -1,6 +0,0 @@
-export MAMBODIR=/home/ATLAS/disipio/development/MAMbo
-source /home/ATLAS/disipio/development/MAMbo/bin/MAMbo-autocompletion.sh
-
-export PATH=${PATH}:${MAMBODIR}/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MAMBODIR}/lib:${MAMBODIR}/MoMA/lib
-
