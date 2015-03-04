9174e73ca012f1db619993e36dbff4229ea8e859
Riccardo Di Sipio <disipio@cern.ch>
2015-03-04 21:31:36 +0000

    fixed setup script

--- MoMA/setup_rootcore.sh
+++ MoMA/setup_rootcore.sh
@@ -5,14 +5,12 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
 setupATLAS
 
-if [ ${USER} == "vscarfon" ]
-then
-  RCDIR=/afs/cern.ch/work/v/vscarfon/public/AnalysisTop-1.8.0
-fi
+[ ${USER} == "vscarfon" ] && RCDIR=/afs/cern.ch/work/v/vscarfon/public/AnalysisTop-1.8.0
+[ ${USER} == "disipio"  ] && RCDIR=/home/ATLAS/disipio/LOCAL_DISK/AnalysisTop-1.8.0
 
 cd ${RCDIR}
 source rcSetup.sh
 
 export ROOTCOREDIR=$ROOTCOREBIN
 
-cd /afs/cern.ch/user/v/vscarfon/public/analysis/MAMbo
+cd ${MAMBODIR}
