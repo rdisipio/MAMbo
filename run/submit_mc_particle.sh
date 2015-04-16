c893763ff1246788bdf5e65d67ca7f83457ce7ae
Riccardo Di Sipio <disipio@cern.ch>
2015-04-16 11:42:57 +0000

    added scripts and xml to run on particle level

---
+++ run/submit_mc_particle.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+analysis=tt_diffxs_8TeV
+outtag=TTbarResolved_resolved
+syst=particle
+
+dsid=117050
+[ ! -z $1 ] && dsid=$1
+
+for ch in el mu
+do
+     echo "INFO: submitting DiTop dsid=$dsid channel ${ch}"
+
+     params=${MAMBODIR}/share/control/analysis_params/${outtag}/mcparticle_${ch}.xml 
+     filelist=${MAMBODIR}/share/control/file_list/${outtag}/mc.DiTop.${dsid}.mc.txt
+
+     for flist in $(ls ${filelist}.*)
+     do
+         batchid=$(echo ${flist} | grep -oE "[^.]+$")
+
+	 tag=${analysis}.mc.DiTop.${dsid}.${ch}.${syst}
+
+         outfile=${syst}/${tag}.histograms.root.${batchid}
+         jobname=${tag}.${batchid}
+       
+	 echo "Submitting ${jobname}"
+         MAMbo-submit.sh -p ${params} -f ${flist} -o ${outfile} -j ${jobname} # -q T3_BO_FAST
+     done
+
+     echo
+done
