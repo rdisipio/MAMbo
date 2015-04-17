c50b7d3636716fd914419f91757442d54f63ed27
Riccardo Di Sipio <disipio@cern.ch>
2015-04-17 22:08:29 +0000

    config files and script for Wjets sherpa

---
+++ run/printSumOfWeights.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os,sys
+
+from ROOT import *
+
+basedir = os.environ['MAMBONTUPLEDIR'] + "/"
+
+file_list = sys.argv[1]
+
+hpath = "mini_fullCutFlow_genWeights"
+i = 1 #all events
+sumw = 0.
+
+f = open( file_list )
+for filename in f.readlines():
+   filename = basedir + filename.strip()
+
+   file = TFile.Open( filename )
+ 
+   h_cutflow_weighted = file.Get( hpath )
+
+   w = h_cutflow_weighted.GetBinContent( i )
+
+   sumw += w
+
+print "Sum of weights: %f" % sumw
