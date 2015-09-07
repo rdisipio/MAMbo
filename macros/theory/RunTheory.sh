8460ed7bab242ead400459f1f52053a089691169
Jiri Kvita <jiri.kvita@cern.ch>
2015-09-07 08:41:57 +0000

    fix

--- macros/theory/RunTheory.sh
+++ macros/theory/RunTheory.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Making top pT predictions by N. Kidonakis..."
+echo "Making predictions by N. Kidonakis..."
 root -l -b -q 'RebinTheory_pTt_aNNLO.C+("data/ptaNNNLO8lhc173.3m.dat")' >& log_pTt_m.txt
 root -l -b -q 'RebinTheory_pTt_aNNLO.C+("data/ptaNNNLO8lhc173.3halfm.dat")' >& log_pTt_halfm.txt
 root -l -b -q 'RebinTheory_pTt_aNNLO.C+("data/ptaNNNLO8lhc173.3-2m.dat")' >& log_pTt_2m.txt
+root -l -b -q 'RebinTheory_yt_aNNLO.C+' >& log_yt.txt
 
 
 echo "Making predictions by Li Lin..."
 root -l -b -q 'RebinTheory_pTtt_NNLO.C+("data/pTtt_8000.dat", 0, false, 0, "recreate")' >& log_pTtt.txt
-root -l -b -q 'RebinTheory_yt_aNNLO.C+' >& log_yt.txt
 root -l -b -q 'RebinTheory_mtt_NNLO.C+' >& log_mtt.txt
 
 
