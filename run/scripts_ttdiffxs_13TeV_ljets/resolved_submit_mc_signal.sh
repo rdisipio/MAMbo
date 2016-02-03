eb0ece9dbb9be513e1020ceaf7edbc8beb926d11
Marino Romano <marino.romano@cern.ch>
2016-02-03 11:09:40 +0000

    Added list of systematics for signal

--- run/scripts_ttdiffxs_13TeV_ljets/resolved_submit_mc_signal.sh
+++ run/scripts_ttdiffxs_13TeV_ljets/resolved_submit_mc_signal.sh
@@ -18,98 +18,102 @@ do
 
 		#paramsdir=${MAMBODIR}/share/control/analysis_params/${outtag}	
 
-		dsid=410000
-		[ ! -z $1 ] && dsid=$1
+		
+		samples=mc.410000.PowhegPythiaEvtGen.e3698_s2608_s2183_r7267_r6282_p2516.${production}_v2.txt
 
-		#decay=nofullhad
-		[ ! -z $2 ] && decay=$2
-
-		if [ $syst == "nocut" ]
-		then
-			template=$PWD/Resolved13TeV.nocut.xml.template
-		else
-			template=$PWD/Resolved13TeV.xml.template
-		fi
-
-		filelistdir=$PWD/filelists_${production}/
-		filelist=$filelistdir/mc.410000.PowhegPythiaEvtGen.e3698_s2608_s2183_r7267_r6282_p2516.${production}_v2.txt
-
-		nomalizationdir=${MAMBODIR}/share/data/NEvents_${production}/
-		nomalizationfile=${MAMBODIR}/share/data/NEvents_${production}/410000.PowhegPythiaEvtGen.e3698_s2608_s2183_r7267_r6282_p2516.${production}_v2.evt.n
-	
-		sendOnce=0
-	#set -x
-
-
-		for ch in el mu
+		[ ! -z $1 ] && samples=$(cat $1)
+		for sample in $samples
 		do
-			if [ $ch == "el" ]
-			then
-				#check if it's a muon systematic
-				if [[ $syst == MUON* ]]
-				then 
-					echo "Muon systematic for el channel: $syst"
-		#			continue
-				fi
+			filelistdir=$PWD/filelists_${production}/
+			filelist=$filelistdir/mc.410000.PowhegPythiaEvtGen.e3698_s2608_s2183_r7267_r6282_p2516.${production}_v2.txt
 
-				
+			dsid=$(echo $sample | cut -d. -f1)
+			normalizationfile=$(echo $sample | sed s/mc.// | sed s/txt/evt.n/)
+			nomalizationdir=${MAMBODIR}/share/data/NEvents_${production}/
+			nomalizationfile=${normalizationdir}/${normalizationfile}
+			
+			if [ $syst == "nocut" ]
+			then
+				template=$PWD/Resolved13TeV.nocut.xml.template
+			else
+				template=$PWD/Resolved13TeV.xml.template
 			fi
-			if [ $ch == "mu" ]
-			then
-				#check if it's a muon systematic
-				if [[ $syst == EG* ]]
-				then 
-					echo "Electron systematic for mu channel: $syst"
-		#			continue
-				fi
 
-				
-			fi			
-			echo "INFO: submitting DiTop dsid=$dsid channel ${ch} decay ${decay}"
+		
+			sendOnce=1
+		#set -x
 
-			[ $ch == "el" ] && ch_tag="electron"	
-			[ $ch == "mu" ] && ch_tag="muon"
 
-			filelist_mc=$filelist
-
-			for flist in $(ls ${filelist}.??)
+			for ch in el mu
 			do
-				batchid=$(echo ${flist} | grep -oE "[^.]+$")
-
-				flist_mc=$( echo ${flist} | sed "s/.${ch}.txt/.mc.txt/" )
-
-				tag=${analysis}.mc.DiTop.${dsid}.${ch}.${syst}.${decay}
-
-				params=$PWD/control/analysis_params/13TeV_ljets_resolved/config/Resolved13TeV.${ch}.${syst}.${decay}.${batchid}.xml
-				cp $template ${params}
-
-				# 	 params=Resolved13TeV_${ch}.${syst}.${decay}.${batchid}.xml
-				#          cp Resolved13TeV.xml.template ${params}
-				if [ $syst != "nocut" ]
+				if [ $ch == "el" ]
 				then
-					sed -i "s|@SYST@|${syst}|"     ${params}
+					#check if it's a muon systematic
+					if [[ $syst == MUON* ]]
+					then 
+						echo "Muon systematic for el channel: $syst"
+			#			continue
+					fi
+
+					
 				fi
-				sed -i "s|@CHANNEL@|${ch_tag}|"     ${params}
-				sed -i "s|@MCFILELIST@|${flist_mc}|"  ${params}
-				sed -i "s|@NORMFILE@|${nomalizationfile}|"  ${params}
-				sed -i "s|@DECAY@|${decay}|"  ${params}
-				mkdir -p output_${production}/${syst}
-				outfile=${syst}/${tag}.histograms.root.${batchid}
-				jobname=${tag}.${batchid}
-
-				echo ${params}  
-				echo ${flist_mc}
-
-				echo "Submitting ${jobname}"
-				MAMbo-submit.sh -p ${params} -f ${flist} -o ${outfile} -j ${jobname}
-				if [ $sendOnce -eq 1 ]
+				if [ $ch == "mu" ]
 				then
-					break
-				fi
+					#check if it's a muon systematic
+					if [[ $syst == EG* ]]
+					then 
+						echo "Electron systematic for mu channel: $syst"
+			#			continue
+					fi
+
+					
+				fi			
+				echo "INFO: submitting DiTop dsid=$dsid channel ${ch} decay ${decay}"
+
+				[ $ch == "el" ] && ch_tag="electron"	
+				[ $ch == "mu" ] && ch_tag="muon"
+
+				filelist_mc=$filelist
+
+				for flist in $(ls ${filelist}.??)
+				do
+					batchid=$(echo ${flist} | grep -oE "[^.]+$")
+
+					flist_mc=$( echo ${flist} | sed "s/.${ch}.txt/.mc.txt/" )
+
+					tag=${analysis}.mc.DiTop.${dsid}.${ch}.${syst}.${decay}
+
+					params=$PWD/control/analysis_params/13TeV_ljets_resolved/config/Resolved13TeV.${ch}.${syst}.${decay}.${batchid}.xml
+					cp $template ${params}
+
+					# 	 params=Resolved13TeV_${ch}.${syst}.${decay}.${batchid}.xml
+					#          cp Resolved13TeV.xml.template ${params}
+					if [ $syst != "nocut" ]
+					then
+						sed -i "s|@SYST@|${syst}|"     ${params}
+					fi
+					sed -i "s|@CHANNEL@|${ch_tag}|"     ${params}
+					sed -i "s|@MCFILELIST@|${flist_mc}|"  ${params}
+					sed -i "s|@NORMFILE@|${nomalizationfile}|"  ${params}
+					sed -i "s|@DECAY@|${decay}|"  ${params}
+					mkdir -p output_${production}/${syst}
+					outfile=${syst}/${tag}.histograms.root.${batchid}
+					jobname=${tag}.${batchid}
+
+					echo ${params}  
+					echo ${flist_mc}
+
+					echo "Submitting ${jobname}"
+					MAMbo-submit.sh -p ${params} -f ${flist} -o ${outfile} -j ${jobname}
+					if [ $sendOnce -eq 1 ]
+					then
+						break
+					fi
+				done
 			done
+
+
+			echo
 		done
-
-
-		echo
 	done
 done
--- run/scripts_ttdiffxs_13TeV_ljets/resolved_submit_merge_data.sh
+++ run/scripts_ttdiffxs_13TeV_ljets/resolved_submit_merge_data.sh
@@ -3,40 +3,44 @@
 analysis=tt_diffxs_13TeV
 #outtag=TTbarResolved_resolved
 
-systs="data  qcd"
+systs="qcd"
 #systs=`cat resolved_kinematic_systematics.dat resolved_scale_systematics.dat | egrep -e "EG|MUONS"`
 #systs=`cat  resolved_scale_systematics.dat | grep lepton`
 decays="nofullhad ljets"
 decays="nofullhad"
-for syst in $systs
+methods="MM MMM"
+for method in $methods
 do
-	
-	#syst=nominal
+	for syst in $systs
+	do
+		
+		#syst=nominal
 
-	#paramsdir=${MAMBODIR}/share/control/analysis_params/${outtag}	
+		#paramsdir=${MAMBODIR}/share/control/analysis_params/${outtag}	
 
-	
-	[ ! -z $1 ] && dsid=$1
+		
+		[ ! -z $1 ] && dsid=$1
 
-	sendOnce=0
-#set -x
-	dsid="AllPeriod"
+		sendOnce=0
+	#set -x
+		dsid="AllPeriod"
 
-	batchid=hadd.$dsid.$decay.$syst
-	script=../jobs/$batchid.sh
-	cat > $script << EOF
-#!/bin/bash
-		#tag=${analysis}.mc.DiTop.${dsid}.${ch}.${syst}.${decay}
-	#	tt_diffxs_13TeV.mc.DiTop.410000.mu.nominal.nofullhad.histograms.root.40
-	hadd -f output/nominal/${analysis}.${syst}.Main.${dsid}.el.Resolved.histograms.root output/nominal/${analysis}.${syst}.Main.00*.el.Resolved.histograms.root
-	hadd -f output/nominal/${analysis}.${syst}.Main.${dsid}.mu.Resolved.histograms.root output/nominal/${analysis}.${syst}.Main.00*.mu.Resolved.histograms.root
-	hadd -f output/nominal/${analysis}.${syst}.Main.${dsid}.co.Resolved.histograms.root output/nominal/${analysis}.${syst}.Main.${dsid}.el.Resolved.histograms.root output/nominal/${analysis}.${syst}.Main.${dsid}.mu.Resolved.histograms.root 
+		batchid=hadd.$dsid.$decay.$syst
+		script=../jobs/$batchid.sh
+		cat > $script << EOF
+	#!/bin/bash
+			#tag=${analysis}.mc.DiTop.${dsid}.${ch}.${syst}.${decay}
+		#	tt_diffxs_13TeV.mc.DiTop.410000.mu.nominal.nofullhad.$method.histograms.root.40
+		hadd -f output/nominal/${analysis}.${syst}.Main.${dsid}.el.Resolved.$method.histograms.root resolvedQCD/${analysis}.${syst}.Main.00*.el.Resolved.$method.histograms.root
+		hadd -f output/nominal/${analysis}.${syst}.Main.${dsid}.mu.Resolved.$method.histograms.root resolvedQCD/${analysis}.${syst}.Main.00*.mu.Resolved.$method.histograms.root
+		hadd -f output/nominal/${analysis}.${syst}.Main.${dsid}.co.Resolved.$method.histograms.root output/nominal/${analysis}.${syst}.Main.${dsid}.el.Resolved.$method.histograms.root output/nominal/${analysis}.${syst}.Main.${dsid}.mu.Resolved.$method.histograms.root 
 
 EOF
-	chmod +x $script
-#	./$script
-		qsub -q T3_BO_LOCAL $script
+		chmod +x $script
+		./$script
+		#	qsub -q T3_BO_LOCAL $script
 
 
 
+	done
 done
