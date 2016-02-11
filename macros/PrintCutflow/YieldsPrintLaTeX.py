8a40d72b6e2ef3186e7cad48caab54ec0d06dffa
Jiri Kvita <jiri.kvita@cern.ch>
2016-02-11 08:49:59 +0000

    new migra

--- macros/PrintCutflow/YieldsPrintLaTeX.py
+++ macros/PrintCutflow/YieldsPrintLaTeX.py
@@ -8,26 +8,55 @@ from ROOT import *
 
 path = '/afs/cern.ch/work/m/mromano/public/Diff13TeV/output/'
 
-#ljets = ['el', 'mu', 'co']
-ljets = [ 'co']
-topos = {'resolved' : '4j2b', 
-         # 'boosted' : '1fj0b' 
+ljets = ['el', 'mu', 'co']
+label = ['\\ejets{}', '\mujets{}', '\\ljets{}']
+
+
+#ljets = [ 'co']
+topos = {#'resolved' : '4j2b', 
+         'boosted' : '1fj1b' 
 }
+
 
 for topo in topos:
 
+  lines = []
+  
+
+  line = ''
+  il = 0
   for ljet in ljets:
-    print '=== %s %s ===' % (topo,ljet,)
+    stop = ' & '
+    if ljet == ljets[-1]:
+      stop = '\\\\n'
+    line = line + ' %s &' % (label[il],)
+    il = il + 1
+  lines.append(line + ' \\\\\n')
+  
+
+  il = 0
+  ip = 0
+  for ljet in ljets:
+    stop = ' & '
+    if ljet == ljets[-1]:
+      stop = '\\\\'
+    line = 'Sample & '
+    '=== %s %s ===' % (topo,label[il],)
     fnamelist = [ ['Diboson' + '_' + ljet, 'Diboson'],
                   ['Stop' + '_' + ljet, 'singletop'],
                   ['Wjets' + '_' + ljet, '$W$+jets'],
                   ['Zjets' + '_' + ljet, '$Z$+jets'],
+                  ['tt_diffxs_13TeV.qcd.Main.AllPeriod.' + ljet + '.Resolved.histograms', 'Multijet'],
                   ['tt_diffxs_13TeV.mc.DiTop.410000.' +  ljet + '.nominal.nofullhad.histograms', '$\\ttbar{}$ signal'],
-                  ['tt_diffxs_13TeV.data.AllPeriod.' + ljet + '.Resolved.histograms', 'Data'] ]
+                  ['tt_diffxs_13TeV.data.Main.AllPeriod.' + ljet + '.Resolved.histograms', 'Data'] ]
+
+
     flist = []
     histos = []
 
     #print ' Cut name ',
+    sum = 0.
+    ip = 0
     for fnamel in fnamelist:
         fname = fnamel[0]
         if fname == fnamelist[-1][0]:
@@ -35,15 +64,35 @@ for topo in topos:
         rfile = TFile(path + fname + '.root', "read")
         flist.append(rfile)
 
+
+        if il == 0:
+          lines.append('')
+
+
         ### !!! to move to passed_*cutflow_mc for MC !!!!
         hname = 'reco/' + topos[topo] + '/topH/pt'
         histo = rfile.Get(hname)
         histos.append(histo)
         val = histo.Integral(-1,-1)
         if fnamel[1].find('Data') < 0:
-          print ' %s & %4.1f \\\\\n' % (fnamel[1], val,),
+          sum = sum + val
+          if il == 0:
+            lines[ip] = lines[ip] + ( ' %s & ' % (fnamel[1], ) )
+          lines[ip] = lines[ip] + ( ' %4.1f %s ' % (val,stop,) )
         else: 
-          print ' %s & %4.0f \\\\\n' % (fnamel[1], val,),
-        
+          if il == 0:
+            lines[ip] = lines[ip] + ( '\\hline %s &' % (fnamel[1],) )
+          lines[ip] = lines[ip] + ( ' %4.0f %s' % (val,stop,) )
+        print ip
+        ip = ip + 1
 
+    ip = ip+1
+    if il == 0:
+      lines.append(' %s & ' % ('Total prediction',) )
+    lines[ip] = lines[ip] + '  %4.1f %s' % ( sum,stop)
+
+    il = il + 1
    
+  # print lines
+  for ln in lines:
+    print ln
--- macros/bootstraptest/TestReplicas.C
+++ macros/bootstraptest/TestReplicas.C
@@ -2,25 +2,41 @@
 
 
 
-  //TString dir = "/afs/cern.ch/user/q/qitek/public/MCsigHalves/BootsTrap/10toys";
-  TString dir = "../../run/";
+  TString dir = "/afs/cern.ch/user/q/qitek/public/MCsigHalves/BootsTrap/1000toys";
+  //  TString dir = "/afs/cern.ch/user/q/qitek/public/MCsigHalves/BootsTrap/5000toys";                                                                                                             // TString dir = "../../run/";
+  
   TString fname = dir + "/histograms_data_el.root";
 
+
+  cout << "Loading libs..." << endl;
 
   gSystem->AddIncludePath(" -I../../../");
   gSystem->AddIncludePath(" -I../../../BootstrapGenerator/");
   gROOT->ProcessLine( ".L ../../../BootstrapGenerator/StandAlone/libBootstrapGenerator.so" );
+
+
+
   TH1DBootstrap * h;
   TFile * f = TFile::Open( fname, "read" );
   //  f->GetObject( "m_boots", h );
-  f->GetObject( "reco_4j2b_tt_m_boots", h );
-  TH1D * hist = h->GetNominal();
-  hist->Print( "all" );
-  int nReplica = h->GetNReplica();
-  for( int i = 0; i < nReplica; ++i )
-  {
+
+  
+  const int N = 5;
+  TString names[N] = {"reco_4j2b_topH_pt_boots", "reco_4j2b_topH_absrap_boots", 
+		     "reco_4j2b_tt_m_boots", "reco_4j2b_tt_pt_boots",  "reco_4j2b_tt_absrap_boots"};
+  
+
+  for (int j = 0; j < N; ++j) {
+    cout << "*** Processing spectrum " << names[j].Data() << endl;
+    f->GetObject( names[j], h );
+    TH1D * hist = h->GetNominal();
+    hist->Print( "all" );
+    int nReplica = h->GetNReplica();
+    for( int i = 0; i < nReplica; ++i ) {
       hist = (TH1D*) h->GetReplica( i );
       hist->Print( "all" );
+    }
   }
+
 }
 
