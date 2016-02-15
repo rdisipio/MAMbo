4af54d447f78e485552e0006569375c97ede5694
Marino Romano <marino.romano@cern.ch>
2016-02-15 16:17:47 +0000

    Added number of events for pdf systemat5ics

---
+++ share/data/NEvents_TTDIFFXS_55/410003.aMcAtNloHerwigppEvtGen.e3964_a766_a777_r6282_p2442.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ATLAS-T3/mromano/testarea/unversioned/Diff13TeV/MAMbo/run/NEvents
+./a.out 410003.aMcAtNloHerwigppEvtGen.e3964_a766_a777_r6282_p2442.list > 410003.aMcAtNloHerwigppEvtGen.e3964_a766_a777_r6282_p2442.evt
+
+
