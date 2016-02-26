ea8c895fe6701157d2328de53cea6c46cb354d05
Federica Fabbri <federica.fabbri@cern.ch>
2016-02-26 14:54:56 +0000

    update sh to launch QCD

--- run/scripts_ttdiffxs_13TeV_ljets/submit_qcd.sh
+++ run/scripts_ttdiffxs_13TeV_ljets/submit_qcd.sh
@@ -3,29 +3,32 @@
 analysis=tt_diffxs_13TeV
 
 sourcedir=${MAMBODIR}/run/scripts_ttdiffxs_13TeV_ljets
-prod="prod35"
+prod="prod55_1802"
 
 for ch in el mu
 do
 #  [[ ${ch} == "el" ]] && stream="Egamma"
 #  [[ ${ch} == "mu" ]] && stream="Muons"
   stream="QCD"
-  for list in `ls ${MAMBODIR}/run/scripts_ttdiffxs_13TeV_ljets/filelists_TTDIFFXS_35/*physics_Main*`
+  for method in MM
   do
-    run=`echo $list | cut -d. -f2`
-    topology=Boosted
-    tag=${analysis}.${stream}.${run}.${ch}.${topology}
+    for list in `ls ${MAMBODIR}/run/scripts_ttdiffxs_13TeV_ljets/filelists_TTDIFFXS_55/*physics_Main*`
+    do
+      run=`echo $list | cut -d. -f2`
+      topology=Boosted
+      tag=${analysis}.${stream}.${run}.${ch}.${topology}.${method}
 
-    echo $run
+      echo $run
 
-    jobname=${tag}
-    paramfile=${sourcedir}/qcd_${topology}_${ch}.xml
-
-    #filelist=${sourcedir}/filelists_TTDIFFXS_3/$list
-    mkdir -p ${MAMBODIR}/run/output/QCD_${prod}
-    outfile=QCD_${prod}/${tag}.histograms.root
-   # echo $outfile
-   ../bin/MAMbo-submit.sh -p ${paramfile} -f ${list} -o ${outfile} -j ${jobname}
+      jobname=${tag}
+      paramfile=${sourcedir}/qcd_${topology}_${ch}_${method}.xml
+    
+      #filelist=${sourcedir}/filelists_TTDIFFXS_3/$list
+      mkdir -p ${MAMBODIR}/run/output/QCD_${prod}
+      outfile=QCD_${prod}/${tag}.histograms.root
+      # echo $outfile
+      MAMbo-submit.sh -p ${paramfile} -f ${list} -o ${outfile} -j ${jobname}
+    done
   done
 
 done
