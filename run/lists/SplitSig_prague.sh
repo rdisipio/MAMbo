0bb7547612b51cef530b0b94236054127f26064a
Jiri Kvita <jiri.kvita@cern.ch>
2016-05-09 12:50:46 +0000

    more prg

---
+++ run/lists/SplitSig_prague.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+split -l 14 list__410000.PowhegPythiaEvtGen.txt split_list__410000.PowhegPythiaEvtGen_a
--- run/scripts_ttdiffxs_13TeV_ljets_prague/PrepareSig_prague.sh
+++ run/scripts_ttdiffxs_13TeV_ljets_prague/PrepareSig_prague.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# assumes you did
+# cd lists ; . MakeFileListRucio.sh ; cd ../
+# cd lists ; ./SplitSig_prague.sh ; cd ../
+
 ./scripts_ttdiffxs_13TeV_ljets_prague/RunSig_prague.py | grep runM > _submit_410000.sh
 split -l 1 _submit_410000.sh _submit_410000_a 
