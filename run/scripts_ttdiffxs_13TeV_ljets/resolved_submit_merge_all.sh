91fa868ee28f202d6906d124ee1d840067747542
Marino Romano <marino.romano@cern.ch>
2016-06-15 10:56:32 +0000

    For nominal only, two new files are created by the merging script: *TotalPredictionNoQCD* and *AllBkgNoQCD* (see TTDIFFXS-66)

--- run/scripts_ttdiffxs_13TeV_ljets/resolved_submit_merge_all.sh
+++ run/scripts_ttdiffxs_13TeV_ljets/resolved_submit_merge_all.sh
@@ -2,9 +2,16 @@
 
 systs="nominal `cat resolved_kinematic_systematics.dat resolved_scale_systematics.dat` Zjets_up Stop_up Diboson_up ttV_up wjets_stat wjets_syst"
 #systs="nominal"
-systs="Diboson_up Stop_up Zjets_up" 
+#systs="Diboson_up Stop_up Zjets_up" 
 channels="el mu co"
-
+#systs="fakes_realEff_stat_D fakes_realEff_stat_U fakes_fakeEff_stat_D fakes_fakeEff_stat_U fakes_fakeEff_MCscale_D fakes_fakeEff_MCscale_U fakes_fakeEff_CR_S"
+systs="nominal `cat resolved_kinematic_systematics.dat resolved_scale_systematics.dat` 
+Zjets_up Stop_up Diboson_up ttV_up 
+wjets_stat wjets_syst 
+fakes_realEff_stat_D fakes_realEff_stat_U fakes_fakeEff_stat_D fakes_fakeEff_stat_U fakes_fakeEff_MCscale_D fakes_fakeEff_MCscale_U fakes_fakeEff_CR_S"
+systs="Fakes_up Zjets_up Diboson_up Stop_up ttV_up"
+#systs="`cat resolved_scale_systematics.dat`"
+systs=nominal
 for  syst in $systs
 do
 	dir="$PWD/output/$syst/"
@@ -25,19 +32,22 @@ do
                         backgroundsamples="\$backgroundsamples tt_diffxs_13TeV.mc.\$b.\$c.$syst.nofullhad.histograms.root"
 #		backgroundsamples="tt_diffxs_13TeV.mc.Wjets.\$c.$syst.nofullhad.histograms.root tt_diffxs_13TeV.mc.Zjets.\$c.$syst.nofullhad.histograms.root tt_diffxs_13TeV.mc.Diboson.\$c.$syst.nofullhad.histograms.root tt_diffxs_13TeV.mc.ttV.\$c.$syst.nofullhad.histograms.root
                 done
+                backgroundsamplesNoQCD=\$backgroundsamples
                 backgroundsamples="\$backgroundsamples tt_diffxs_13TeV.qcd.Main.AllPeriod.\$c.Resolved.MM.histograms.root"
 		signalsample="tt_diffxs_13TeV.mc.DiTop.410000.\$c.$syst.nofullhad.histograms.root"
 		#do all background
 		hadd -f tt_diffxs_13TeV.mc.AllBkg.\$c.$syst.nofullhad.histograms.root \$backgroundsamples
+		hadd -f tt_diffxs_13TeV.mc.AllBkgNoQCD.\$c.$syst.nofullhad.histograms.root \$backgroundsamplesNoQCD
 		#do total prediction
 		hadd -f tt_diffxs_13TeV.mc.TotalPrediction.\$c.$syst.nofullhad.histograms.root \$signalsample tt_diffxs_13TeV.mc.AllBkg.\$c.$syst.nofullhad.histograms.root
+		hadd -f tt_diffxs_13TeV.mc.TotalPredictionNoQCD.\$c.$syst.nofullhad.histograms.root \$signalsample tt_diffxs_13TeV.mc.AllBkgNoQCD.\$c.$syst.nofullhad.histograms.root
 done
 		#hadd -f AllBkg_co.root AllBkg_el.root AllBkg_mu.root
 		
                 #hadd -f TotalPrediction_co.root TotalPrediction_el.root TotalPrediction_mu.root
 EOF
 	chmod +x $script
-#	./$script
-	bsub -oe -oo $log -J $batchid -q T3_BO_LOCAL $script
+	./$script
+#	bsub -oe -oo $log -J $batchid -q T3_BO_LOCAL $script
 
 done
