0cbbf815fa8bf1dfedd1808e7a688ea0be878ba8
Jiri Kvita <jiri.kvita@cern.ch>
2016-06-20 11:37:49 +0000

    dpm double slash bug

--- run/lists/MakeFileListRucio_Wjets.sh
+++ run/lists/MakeFileListRucio_Wjets.sh
@@ -9,6 +9,6 @@ for list in  bg_Wjets_new ; do
   for sample in `cat ${list}.txt` ; do
     tag=`echo $sample | cut -d \. -f 4,5`
     echo "Sample: $sample tag: $tag"
-    rucio list-file-replicas --rse PRAGUELCG2_LOCALGROUPDISK --proto root ${sample} | awk '/root:/{print $(NF-1)}' > list_${uid}_${tag}_v8.txt
+    rucio list-file-replicas --rse PRAGUELCG2_LOCALGROUPDISK --proto root ${sample} | awk '/root:/{print $(NF-1)}' | sed 's|/dpm/|//dpm/|g' > list_${uid}_${tag}_v8.txt
   done
 done
