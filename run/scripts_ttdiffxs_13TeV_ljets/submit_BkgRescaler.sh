5305c603cef620ec4e955f23e9bb107e8c93a00a
Federica Fabbri <federica.fabbri@cern.ch>
2016-07-12 07:49:54 +0000

    Updated submit_BkgRescaler.sh to create link for other bkg

--- run/scripts_ttdiffxs_13TeV_ljets/submit_BkgRescaler.sh
+++ run/scripts_ttdiffxs_13TeV_ljets/submit_BkgRescaler.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-backgrounds="Diboson Zjets ttV" #single top is special
+backgrounds="Diboson Zjets ttV Stop" #single top is special
 channels="el mu co"
+production=TTDIFFXS_62_v20
 set -x
 skipStop=0
 
@@ -10,10 +11,23 @@ for c in $channels
 do
         for b in $backgrounds
         do
-		nominal="output/nominal/tt_diffxs_13TeV.mc.$b.$c.nominal.histograms.root"
+		nominal="output/${production}/nominal/tt_diffxs_13TeV.mc.$b.$c.nominal.histograms.root"
 
-                path="output/${b}_up/tt_diffxs_13TeV.mc.$b.$c.${b}_up.histograms.root"
-                cp -v $nominal $path
+                path="output/${production}/${b}_up/tt_diffxs_13TeV.mc.$b.$c.${b}_up.histograms.root"
+
+
+                for link in $backgrounds
+                do
+                  if [[ $link == $b ]]; then 
+                     continue;
+                  fi
+                  ln -sfn ${MAMBODIR}/run/output/${production}/nominal/tt_diffxs_13TeV.mc.$link.$c.nominal.histograms.root ${MAMBODIR}/run/output/${production}/${b}_up/tt_diffxs_13TeV.mc.$link.$c.${b}_up.histograms.root
+                done
+                  if [[ $b == "Stop" ]]; then
+                     continue;
+                  fi
+                
+                cp $nominal $path
               #cp -v $path.backup $path
                # continue
                 batchid="bkgRescaler.$c.$b"
@@ -26,7 +40,7 @@ A
                 chmod +x $script
                ./$script
            #             bsub -oe -oo $log -J $batchid -q T3_BO_LOCAL $script
-
+         
         done
 	if [ $skipStop != "0" ]
 	then
@@ -44,14 +58,14 @@ A
                 roots=""
                 for b in $stopProcesses
                 do
-			nominal="output/nominal/tt_diffxs_13TeV.mc.\$b.$c.nominal.histograms.root"
-                        path="output/Stop_up/tt_diffxs_13TeV.mc.\$b.$c.Stop_up.histograms.root"
+			nominal="output/${production}/nominal/tt_diffxs_13TeV.mc.\$b.$c.nominal.histograms.root"
+                        path="output/${production}/Stop_up/tt_diffxs_13TeV.mc.\$b.$c.Stop_up.histograms.root"
                         cp -v \$nominal \$path             
 			
                         root -b -q "boosted_BkgRescaler.C(\"\$path\", \"\$b\" )"
                         roots="\$roots \$path"
                 done  
-                hadd -f output/Stop_up/tt_diffxs_13TeV.mc.Stop.$c.Stop_up.histograms.root \$roots
+                hadd -f output/${production}/Stop_up/tt_diffxs_13TeV.mc.Stop.$c.Stop_up.histograms.root \$roots
                
 A
     
