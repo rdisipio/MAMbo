efb856c7defa87ef0b9b434e2e93b7a6dcfc2896
Marino Romano <marino.romano@cern.ch>
2016-07-19 14:54:20 +0000

    First macro for lumi rescaling

---
+++ run/scripts_ttdiffxs_13TeV_ljets/resolved_LumiRescaler.C
@@ -0,0 +1,77 @@
+void resolved_LumiRescaler(string filename ){
+  
+  TFile *f = new TFile(filename.c_str(), "update");
+ // TFile *f_out = new TFile(newfilename.c_str());
+  
+  string RegionNameArray[]={"4j2b","cutflow"};
+  std::vector<std::string> RegionName;
+  RegionName.assign(RegionNameArray,RegionNameArray+2);
+
+  double sf = 3158/3209;
+  //   string ParticleNameArray[]={"topH","lep","smallJ","met","bjet","largejet"};
+  //   std::vector<std::string> ParticleName;
+  //   ParticleName.assign(ParticleNameArray,ParticleNameArray+6);
+  //   
+  //   
+  
+  //reweight cutflow
+  
+  // TH1D * h_cutflow = (TH1D*) f->Get( "LPLUSJETS_cutflow_reco_weighted")->Clone();
+  TH1F * h_cutflow = (TH1F*) f->Get( "LPLUSJETS_cutflow_reco_weighted");
+  float before =  h_cutflow->Integral();
+  h_cutflow->Scale( sf );
+  f->cd();
+  // f->Delete("LPLUSJETS_cutflow_reco_weighted;1");
+  // h_cutflow->Write("LPLUSJETS_cutflow_reco_weighted");
+  
+  for(unsigned int j = 0; j<RegionName.size(); j++){
+    f->cd(("reco/"+RegionName[j]).c_str());
+    
+    //Iteration on directories in Reco
+    TListIter ParticleName(gDirectory->GetListOfKeys());
+    while( TObject* tmp_dir = ParticleName.Next()){
+      string currentParticleName=tmp_dir->GetName();
+      
+      cout<<currentParticleName<<endl;
+      string path = "reco/"+RegionName[j]+"/"+currentParticleName;
+      f->cd(path.c_str());
+      TListIter Elements(gDirectory->GetListOfKeys());
+      
+      //Iteration on histogram in each subdirectory 
+      string first=""; 
+      int counter=0;
+      for (int l=0;;l++){
+           
+	
+	
+	bool loop = false;
+	TObject* tmp = Elements.Next();
+	
+	if (!tmp) break;
+	string name = tmp->GetName();
+	
+	if( first == name) break;
+	// TH1F* thisHisto = (TH1F*) f->Get((path+"/"+name).c_str())->Clone();
+	TH1F* thisHisto = (TH1F*) f->Get((path+"/"+name).c_str());
+	cout <<  "Rescaling " << thisHisto->GetName() << " by " << sf << endl;
+	thisHisto->Scale( sf );
+	
+	if(loop == true) {
+	  
+	  gDirectory->Delete((name+";1").c_str());
+	  thisHisto->Write((name).c_str());
+	  if(counter==0) first = name;
+	  counter++;
+	}
+	
+	
+      }  
+      
+    }
+  }
+  
+  cout << "Integral before rescaling" << before << endl;
+  cout << "Integral before closing: " << ((TH1D*) f->Get( "LPLUSJETS_cutflow_reco_weighted"))->Integral() << endl;
+ f->Write(); 
+ f->Close();
+}
---
+++ run/scripts_ttdiffxs_13TeV_ljets/submit_LumiRescaler.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# set -x
+patterns="DiTop Wjets Zjets Diboson ttV Stop"
+
+rm -v lumiList.txt
+
+
+list=`find output/Stop_up -name "tt_diffxs_13TeV.mc.DiTop*.root" -o -name "tt_diffxs_13TeV.mc.Wjets*.root" -o -name "tt_diffxs_13TeV.mc.Zjets*.root" -o -name "tt_diffxs_13TeV.mc.Diboson*.root" -o -name "tt_diffxs_13TeV.mc.ttV*.root" -o -name "tt_diffxs_13TeV.mc.Stop*.root" -type f`
+for f in $list
+do
+        echo "$f"
+        if [ -h $f ] #is a symbolic link
+        then
+                echo "it's a link!"
+        else
+                echo $f >> lumiList.txt
+                filename=`echo $f | cut -d/ -f3 | sed s#/#_#g` 
+                batchid="lumiRescaler.$filename"
+                script="../jobs/$batchid.sh"
+                log="../logs/$batchid.log"
+                cat > $script << A
+        root -b -q 'resolved_LumiRescaler.C("$f" )'
+
+A
+        fi
+
+        # chmod +x $script
+        #./$script
+        #             bsub -oe -oo $log -J $batchid -q T3_BO_LOCAL $script
+
+done
