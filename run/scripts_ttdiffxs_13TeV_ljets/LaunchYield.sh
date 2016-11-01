1fed72c9822c8f3d1042558c3684e7e936f9e9d8
Federica Fabbri <federica.fabbri@cern.ch>
2016-11-01 12:00:50 +0000

    Script to launch yield for boosted

--- run/scripts_ttdiffxs_13TeV_ljets/LaunchYield.sh
+++ run/scripts_ttdiffxs_13TeV_ljets/LaunchYield.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#for sample in "410000" "Wjets" "Zjets" "Diboson" "Stop" "QCD" "ttV" 
-for sample in "TotalPrediction" 
+for sample in "410000" "Wjets" "Zjets" "Diboson" "Stop" "QCD" "ttV" "TotalPrediction"
+#for sample in "Wjets" 
 do
-   for channel in "el" "mu" "co" 
+   for channel in co 
    do
-      ../../bin/MAMbo-makeUncertaintyBand.py -c ../control/analysis_params/13TeV_ljets_boosted/Uncertainty_${sample}_${channel}_statsyst.xml -x ${sample}.${channel}.uncertainty.xml -o ${sample}.${channel}.uncertainty.histograms.root
+      ../../bin/MAMbo-makeUncertaintyBand.py -c ../control/analysis_params/13TeV_ljets_boosted/Uncertainty_${sample}_${channel}_statsys_cutflow.xml -x ${sample}.${channel}.uncertainty.xml -o ${sample}.${channel}.uncertainty.histograms.root
 
      echo $channel
      ../../bin/MakeYields_fromxml.py -i ${sample}.${channel}.uncertainty.xml -s ${sample}
